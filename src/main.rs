fac826200143348085e660052a0357aa5d514d0e
Zytronium <dastelljes@outlook.com>
2025-12-12 12:20:19 -0600

    Add `GameState` to centralize game data; refactor match and plane management logic

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::{HashMap, HashSet};
 use std::time::Duration;
 use uuid::Uuid;
 
@@ -24,60 +25,145 @@ fn main() {
     sim_user_2.link(&mut sim_plane_2);
 
     println!("All planes online and linked. Preparing to start match...");
-    // test();
 }
 
-fn test() {
-    // Create users
-    let user1 = User::new("Player1".to_string());
-    let user2 = User::new("Player2".to_string());
+type MatchId = Uuid;
+type PlaneId = Uuid;
+type UserId = Uuid;
 
-    // Create planes
-    let mut plane1 = Plane::new("F16".to_string(), Some(user1.id));
-    let mut plane2 = Plane::new("MiG29".to_string(), Some(user2.id));
+struct GameState {
+    matches: HashMap<MatchId, Match>,
+    ongoing_matches: HashSet<MatchId>,
+    planes: HashMap<PlaneId, Plane>,
+    users: HashMap<UserId, User>,
+}
 
-    // Create match
-    let mut game_match = Match::new();
-
-    // Register planes as online
-    plane1.register();
-    plane2.register();
-
-    // Join one plane to match
-    plane1.join_match(game_match.id);
-
-    // Add planes to match (store IDs)
-    game_match.planes.push(plane1.id);
-    game_match.planes.push(plane2.id);
-
-    // Print match state
-    println!("Match state: {}", game_match.state);
-    println!("Number of planes: {}", game_match.planes.len());
-    for plane_id in game_match.planes.iter() {
-        println!("Plane id in match: {}", plane_id);
+impl GameState {
+    fn new() -> Self {
+        Self {
+            matches: HashMap::new(),
+            ongoing_matches: HashSet::new(),
+            planes: HashMap::new(),
+            users: HashMap::new(),
+        }
     }
 
-    // If you want plane details, you need a lookup table (e.g. HashMap<Uuid, Plane>) or print from variables:
-    println!(
-        "Plane1: {}, Online: {}, Joined: {}, id: {}",
-        plane1.name, plane1.online, plane1.is_joined, plane1.id
-    );
-    println!(
-        "Plane2: {}, Online: {}, Joined: {}, id: {}",
-        plane2.name, plane2.online, plane2.is_joined, plane2.id
-    );
+    fn create_match(&mut self, m: Match) -> MatchId {
+        let id = m.id;
+        if m.state == "ONGOING" {
+            self.ongoing_matches.insert(id);
+        }
+        self.matches.insert(id, m);
+        id
+    }
+
+    fn set_match_state(&mut self, match_id: MatchId, new_state: &str) {
+        let m = self.matches.get_mut(&match_id).expect("match not found");
+        m.state = new_state.to_string();
+
+        if m.state == "ONGOING" {
+            self.ongoing_matches.insert(match_id);
+        } else {
+            self.ongoing_matches.remove(&match_id);
+        }
+    }
+
+    fn list_ongoing_match_ids(&self) -> impl Iterator<Item = &MatchId> {
+        self.ongoing_matches.iter()
+    }
+
+    fn get_match(&self, match_id: MatchId) -> Option<&Match> {
+        self.matches.get(&match_id)
+    }
 }
 
 struct Plane {
-    id: Uuid,
+    id: PlaneId,
     name: String,
-    pilot_id: Option<Uuid>,
-    joined_match_id: Option<Uuid>,
+    pilot_id: Option<UserId>,
+    joined_match_id: Option<MatchId>,
     online: bool,
     is_joined: bool,
     is_disqualified: bool,
     hits: u16,
     hits_taken: u16,
+}
+
+impl Plane {
+    fn new(name: String, pilot_id: Option<Uuid>) -> Plane {
+        if name.is_empty() {
+            panic!("Plane name cannot be empty");
+        }
+
+        Plane {
+            id: Uuid::new_v4(),
+            name,
+            pilot_id,
+            joined_match_id: None,
+            online: false,
+            is_joined: false,
+            is_disqualified: false,
+            hits: 0,
+            hits_taken: 0,
+        }
+    }
+
+    fn register(&mut self) {
+        self.online = true;
+        println!("Plane {} registered as online.", self.name);
+    }
+
+    fn disconnect(&mut self) {
+        self.online = false;
+        self.is_joined = false;
+        self.joined_match_id = None;
+        // todo: consider disqualifying or resetting disqualified flag
+        println!("Plane {} disconnected.", self.name);
+    }
+
+    fn join_match(&mut self, state: &mut GameState, match_id: MatchId) {
+        let m = state.matches.get_mut(&match_id).expect("match not found");
+        if m.state == "ACTIVE" {
+            panic!("Cannot join an active match..")
+        }
+        if m.state == "ENDED" {
+            panic!("Cannot join an ended match.")
+        }
+        if !m.planes.contains(&self.id) {
+            m.planes.push(self.id);
+        } else {
+            println!("Plane {} tried to join a match twice!", self.name);
+        }
+        // todo: check if plane is already in another match
+
+        self.is_joined = true;
+        self.joined_match_id = Some(match_id);
+        println!("Plane {} joined match {}", self.name, match_id);
+    }
+
+    fn leave_match(&mut self) {
+        self.is_joined = false;
+        self.joined_match_id = None;
+        // todo: remove plane from match
+        println!("Plane {} left match", self.name);
+    }
+
+    fn set_pilot(&mut self, pilot_id: Uuid) {
+        if self.pilot_id.is_some() {
+            panic!("Plane pilot id already set. To reset, use reset_pilot()");
+        }
+        if self.is_joined {
+            panic!("Cannot set pilot while joined to match.");
+        }
+        self.pilot_id = Some(pilot_id);
+    }
+
+    fn reset_pilot(&mut self) {
+        if self.is_joined {
+            panic!("Cannot reset pilot while joined to match.");
+        }
+        self.pilot_id = None;
+    }
 }
 
 struct User {
@@ -124,69 +210,5 @@ impl Match {
             duration: Duration::from_secs(0),
             state: String::from("WAITING"),
         }
-    }
-}
-
-impl Plane {
-    fn new(name: String, pilot_id: Option<Uuid>) -> Plane {
-        if name.is_empty() {
-            panic!("Plane name cannot be empty");
-        }
-
-        Plane {
-            id: Uuid::new_v4(),
-            name,
-            pilot_id,
-            joined_match_id: None,
-            online: false,
-            is_joined: false,
-            is_disqualified: false,
-            hits: 0,
-            hits_taken: 0,
-        }
-    }
-
-    fn register(&mut self) {
-        self.online = true;
-        println!("Plane {} registered as online.", self.name);
-    }
-
-    fn disconnect(&mut self) {
-        self.online = false;
-        self.is_joined = false;
-        self.joined_match_id = None;
-        // todo: consider disqualifying or resetting disqualified flag
-        println!("Plane {} disconnected.", self.name);
-    }
-
-    fn join_match(&mut self, match_id: Uuid) {
-        self.is_joined = true;
-        self.joined_match_id = Some(match_id);
-        // todo: add plane to match
-        println!("Plane {} joined match {}", self.name, match_id);
-    }
-
-    fn leave_match(&mut self) {
-        self.is_joined = false;
-        self.joined_match_id = None;
-        // todo: remove plane from match
-        println!("Plane {} left match", self.name);
-    }
-
-    fn set_pilot(&mut self, pilot_id: Uuid) {
-        if self.pilot_id.is_some() {
-            panic!("Plane pilot id already set. To reset, use reset_pilot()");
-        }
-        if self.is_joined {
-            panic!("Cannot set pilot while joined to match.");
-        }
-        self.pilot_id = Some(pilot_id);
-    }
-
-    fn reset_pilot(&mut self) {
-        if self.is_joined {
-            panic!("Cannot reset pilot while joined to match.");
-        }
-        self.pilot_id = None;
     }
 }
